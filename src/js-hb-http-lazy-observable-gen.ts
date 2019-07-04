f7e5f5c761d23a3a693da3d12d5169a55b8eee47
hailtondecastro <63315947368@SERPRO1556348V1.SERPRO-RLSL>
2019-07-03 22:45:23 -0300

    Refactory for english, minor changes

--- src/js-hb-http-lazy-observable-gen.ts
+++ src/js-hb-http-lazy-observable-gen.ts
@@ -1,6 +1,9 @@
 import { Observable } from 'rxjs/Observable';
 import { HttpResponse } from '@angular/common/http';
 
+/**
+ * Adapter for your application.
+ */
 export interface IJsHbHttpLazyObservableGen {
 	generateHttpObservable(signatureStr: string): Observable<HttpResponse<Object>>;
 }
